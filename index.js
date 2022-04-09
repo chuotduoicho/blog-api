7aef125b2c5156ef77ffd23cc479a8cd44f6fe2d
Duc Tai <vo1122000@gmail.com>
2022-04-09 12:10:23 +0700

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
@@ -10,9 +11,10 @@ const multer = require("multer");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-
+console.log(PORT);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
